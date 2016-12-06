716248d0f0c9d78c763b3249fe189972a79d51b5
mimusyst <mimu@mimsys.net>
2016-12-06 14:55:18 +0900

    del

--- 御影百合絵からのお願い.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $n = trim(fgets(STDIN));
-    $n += trim(fgets(STDIN));
-    $n += trim(fgets(STDIN));
-    echo $n."\n";
-?>
--- 春日みちるからのSOS.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $s = trim(fgets(STDIN));
-    if(strpos($s,"help") !== false){
-        $s = "SOS";
-    }
-    echo $s."\n";
-?>
